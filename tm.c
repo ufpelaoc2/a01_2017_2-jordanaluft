e66a36d6ea478cf476c94c6bf514997f82c97c46
jordana <jordana.sluft@gmail.com>
2017-10-16 23:16:53 -0200

    implementado nivell1

--- tm.c
+++ tm.c
@@ -4,10 +4,15 @@
 uint32_t tempo_medio(uint32_t acesso_l1, float t_miss_l1, uint32_t acesso_l2, float t_miss_l2, uint32_t acesso_l3, float t_miss_l3, uint32_t acesso_mem){
   float tempo_medio;
 
+  // nao ha acesso ao nivel l1 - acessos apenas ate memoria principal
   if (acesso_l1 == 0){
     tempo_medio = acesso_mem;
     return tempo_medio;
   }
 
-
+  // acessos ate nivel l1
+  else if (acesso_l2 == 0){
+    tempo_medio = ceil(acesso_l1 +  acesso_mem * (t_miss_l1 / 100));
+    return tempo_medio;
+  }
 }
